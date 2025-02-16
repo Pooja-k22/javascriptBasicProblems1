e1c5ae93f8d97fe608a91c4ab30bc46a974f9e97
Pooja-k22 <poojakrishnan720@gmail.com>
2025-02-17 02:56:40 +0530

    Added only my specific file

---
+++ prog6.js
@@ -0,0 +1,71 @@
+//1 writ a program betw swap no
+a=20
+b=40
+
+
+c=a
+a=b
+b=c
+console.log(`a= ${a} and b=${b}`);
+
+console.log('-----------------------');
+a=2
+b=3
+
+a=a+b
+b=a-b
+a=a-b
+console.log(`a= ${a} and b=${b}`);
+
+console.log('------------------');
+
+//2 write a program person eligible or not
+age=5
+if(age>=18){
+    console.log('eligible');
+    
+}
+else{
+    console.log('not eligible');
+    
+}
+
+console.log('------------------');
+
+//3 number is positive or negative
+
+n1=0
+if(n1<0){
+    console.log('num is negative');
+    
+}
+else if(n1==0){
+    console.log('neither pos or neg');
+    
+}
+else{
+    console.log('num is positive');
+    
+}
+console.log('------------------');
+console.log(n1>0?'pos no':'neg no');
+
+
+console.log('------------------');
+//4 no is even or odd
+
+n2=0
+if(n2%2==0){
+    console.log('even no');
+    
+}
+
+else{
+    console.log('odd no');
+    
+}
+
+
+
+
+
