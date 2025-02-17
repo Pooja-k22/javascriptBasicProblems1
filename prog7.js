94963d28623c011766062af776bd26bba967e518
Pooja-k22 <poojakrishnan720@gmail.com>
2025-02-18 00:28:23 +0530

    second commit

---
+++ prog7.js
@@ -0,0 +1,52 @@
+
+//5 greatest among 2 nums
+
+n1=100
+n2=200
+if(n1>n2){
+    console.log(`n1 is greatest no`);
+    
+}
+else{
+    console.log(`n2 is greatest no`);
+    
+}
+
+// 6 display 2nd largest no amg 3 given nos
+n1=100
+n2=200
+n3=400
+
+if(n1>n2 && n1<n3){
+    console.log(`n1 is 2nd greatest = ${n1}`);
+    
+}
+else if(n2>n1 && n2<n3){
+    console.log(`n2 is 2nd greatest = ${n2}`);
+}
+else if(n3>n1 && n3<n2){
+    console.log(`n3 is 2nd greatest  = ${n3}`);
+}
+
+console.log(`-----------------------`);
+
+//7  arrange in descending order
+
+n1=100
+n2=20
+n3=400
+
+if(n1>n2 && n1>n3) {
+    console.log(n1);
+   
+}
+else if(n2<n1 && n2>n3){
+    console.log(n2);
+
+}
+else if(n3<n1 && n2<n3){
+    console.log(n3);
+
+}
+
+
